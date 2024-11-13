3f32b388176be46152b7afda078b424942eec08d
akisliy <oleksandr.kyslyi@onix-systems.com>
2024-11-13 19:34:28 +0200

    project init

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -2,13 +2,15 @@ import i18n from 'i18next';
 import Backend from 'i18next-fs-backend';
 import path from 'path';
 
+const rootDir = path.resolve(__dirname, '..');
+
 i18n
   .use(Backend)
   .init({
     fallbackLng: 'en',
     lng: 'en',
     backend: {
-      loadPath: path.join(__dirname, '../locales/{{lng}}/translation.json')
+      loadPath: path.join(rootDir, 'locales', '{{lng}}', 'translation.json')
     },
     interpolation: {
       escapeValue: false
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
+import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,6 +7,9 @@ dotenv.config();
 import routes from './routes';
 
 const bot = new TelegramBot(process.env.BOT_TOKEN || '', { polling: true });
+const app = express();
+
+app.use(express.json());
 
 bot.setMyCommands([
   { command: '/start', description: 'start' },
@@ -18,5 +22,19 @@ bot.setMyCommands([
 ]);
 
 routes(bot);
+
+app.get('/', (req, res) => {
+  res.send('Привет, Vercel с TypeScript!');
+});
+
+app.post(`/bot${process.env.BOT_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
 
 export default bot;
